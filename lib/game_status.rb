d736b61e76adc2f2cf9420bc0e75d6bfae0d43fc
Laura Nadolski <laura.nadolski@gmail.com>
2018-03-31 04:45:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,9 +33,9 @@ def won?(board)
        ((position_1 == "O") &&
        (position_2 == "O") &&
        (position_3 == "O"))
-       win_combinations
+       return win_combinations
        else
-         false
+         return false
      end
    end
  end
