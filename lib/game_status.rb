cbd98480ba5a8181515d8c1dffb754299a50ed32
Laura Nadolski <laura.nadolski@gmail.com>
2018-03-31 05:30:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,9 +20,9 @@ def won?(board)
   
 WIN_COMBINATIONS[0].each do |win_combination|
      
-     win_index_1 = win_combination
-     win_index_2 = win_combination
-     win_index_3 = win_combination
+     win_index_1 = win_combination[0]
+     win_index_2 = win_combination[1]
+     win_index_3 = win_combination[2]
      
      position_1 = board[win_index_1]
      position_2 = board[win_index_2]
